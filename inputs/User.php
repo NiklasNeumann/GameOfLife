7a738bc255a344eeea1cc78b5bad8e8812da793d
Kevin Winkler <kevin.winkler@cn-consult.eu>
2019-10-09 16:51:44 +0200

    Style fixed documentation and text output

--- inputs/User.php
+++ inputs/User.php
@@ -5,27 +5,34 @@ namespace GameOfLife\inputs;
 use GameOfLife\Field;
 use Ulrichsg\Getopt;
 
+/**
+ * Fills the board from user input.
+ *
+ * With the function addOptions, you can add Options from outside of the gameoflife.php file.
+ * The function fillField can be used to fill the $field variable in Field.php with the wished field-stats.
+ * @package GameOfLife\inputs
+ */
 class User extends BaseInput
 {
     /**
-     * Add options
+     * Adds options.
      * Allows to add options to the variable $options in the gameoflife.php file.
-     * @param Getopt $_options
+     * @param Getopt $_options Option manager to check for optional arguments.
      */
     public function addOptions(Getopt &$_options)
     {
     }
 
     /**
-     * Fills field
+     * Fills a field.
      *
-     * @param Field $_field Field to fill.
-     * @param Getopt $_options
+     * @param Field $_field Board to prepare.
+     * @param Getopt $_options Option manager to check for optional arguments.
      */
     public function fillField(Field &$_field, Getopt $_options)
     {
-        echo "To change the state of a cell type in the coordinates of the cell separated by a comma.\n";
-        echo "Additionally the state can be added.";
+        echo "To change a cell type the coordinates and optionally the state separated by commas";
+        echo "and hit enter\n";
         echo "e.g. \"5,14,1\" creates a living cell at 5|14.\n";
         echo "If you are finished type exit\n";
         while(1)
